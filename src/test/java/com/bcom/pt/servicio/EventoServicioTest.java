8e9e63359e2a611d4b28d3cd37db17824dd02c54
Piza, Juan <jpiza@cvent.com>
2021-05-16 11:07:22 -0500

    Migra EventoServicioTest a Junit5

--- src/test/java/com/bcom/pt/servicio/EventoServicioTest.java
+++ src/test/java/com/bcom/pt/servicio/EventoServicioTest.java
@@ -3,22 +3,22 @@ package com.bcom.pt.servicio;
 import com.bcom.pt.entidad.Evento;
 import com.bcom.pt.repositorio.EventoRepositorio;
 import com.bcom.pt.repositorio.UsuarioRepositorio;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDateTime;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-@RunWith(MockitoJUnitRunner.class)
+@ExtendWith(MockitoExtension.class)
 public class EventoServicioTest {
 
     @Mock
